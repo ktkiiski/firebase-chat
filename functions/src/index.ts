a0be5bde2493babbc9f02a8c94f893ab5cd9f737
Kimmo Kiiski <kimmo@kii.ski>
2019-03-30 15:02:25 +0200

    Integrate React with Firebase Cloud Functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,25 +3,16 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const info = functions.https.onRequest((request, response) => {
-  response.setHeader('Content-Type', 'text/html');
-  response.send(`<!DOCTYPE HTML>
-    <html>
-    <head>
-      <title>Chat</title>
-    </head>
-    <body>
-      <pre>INFO
-      baseUrl: ${JSON.stringify(request.baseUrl)}
-      hostname: ${JSON.stringify(request.hostname)}
-      method: ${JSON.stringify(request.method)}
-      path: ${JSON.stringify(request.path)}
-      query: ${JSON.stringify(request.query)}
-      params: ${JSON.stringify(request.params)}
-      url: ${JSON.stringify(request.url)}
-      originalUrl: ${JSON.stringify(request.originalUrl)}
-      </pre>
-    </body>
-    </html>
-  `);
+export const api = functions.https.onRequest((request, response) => {
+  response.setHeader('Content-Type', 'application/json');
+  response.send(JSON.stringify({
+    method: JSON.stringify(request.method),
+    baseUrl: JSON.stringify(request.baseUrl),
+    hostname: JSON.stringify(request.hostname),
+    path: JSON.stringify(request.path),
+    query: JSON.stringify(request.query),
+    params: JSON.stringify(request.params),
+    url: JSON.stringify(request.url),
+    originalUrl: JSON.stringify(request.originalUrl),
+  }));
 });
