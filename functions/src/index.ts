8bbf589294a5d5ad2e78cf6c1a672494b2b29195
Kimmo Kiiski <kimmo@kii.ski>
2019-03-30 14:42:33 +0200

    Proxy requests from React script to served Firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,25 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const info = functions.https.onRequest((request, response) => {
+  response.setHeader('Content-Type', 'text/html');
+  response.send(`<!DOCTYPE HTML>
+    <html>
+    <head>
+      <title>Chat</title>
+    </head>
+    <body>
+      <pre>INFO
+      baseUrl: ${JSON.stringify(request.baseUrl)}
+      hostname: ${JSON.stringify(request.hostname)}
+      method: ${JSON.stringify(request.method)}
+      path: ${JSON.stringify(request.path)}
+      query: ${JSON.stringify(request.query)}
+      params: ${JSON.stringify(request.params)}
+      url: ${JSON.stringify(request.url)}
+      originalUrl: ${JSON.stringify(request.originalUrl)}
+      </pre>
+    </body>
+    </html>
+  `);
+});
